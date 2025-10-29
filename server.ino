405f30ef440594a15d4bdb29fc979da33a3146aa
Nicholas Lediet <nicklediet@gmail.com>
2025-10-28 23:19:45 -0400

    Update server sketch

--- server.ino
+++ server.ino
@@ -53,6 +53,92 @@ void setup() {
   Serial.println(WiFi.localIP());
 }
 
+void writeJsonBody(WiFiClient client) {
+  if(ledState) {
+    client.write("{\"isOn\": true}\r\n");
+  } else {
+    client.write("{\"isOn\": false}\r\n");
+  }
+}
+
+void loop() {
+  // Listen for incoming clients
+  WiFiClient client = server.available();
+  
+  if(client) {
+    if(client.connected()) {
+        client.write("HTTP/1.1 200 OK\r\n");
+        client.write("Content-Type: text/html\r\n");
+        client.write("Access-Control-Allow-Origin: *\r\n");
+        client.write("\r\n");  // End of headers
+      if(!ledState) {
+        ledState = true;
+        digitalWrite(LED_GPIO, HIGH);
+      } else {
+        ledState = false;
+        digitalWrite(LED_GPIO, LOW);
+      }
+      writeJsonBody(client);
+      // Serial.println("Client request recieved!");
+      // // Serial.println("Connected to client!");
+      // client.write("HTTP/1.1 200 OK\r\n");
+      // client.write("Content-Type: text/html\r\n");
+      // // client.write("Connection: close\r\n");
+      // client.write("\r\n");  // End of headers
+
+      // client.write("<!DOCTYPE html>\r\n");
+      // client.write("<html>\r\n");
+      // client.write("<head><title>Hello</title></head>\r\n");
+      // client.write("<body>\r\n");
+      // client.write("<h1>Hello, World!</h1>\r\n");
+      // client.write("</body>\r\n");
+      // client.write("</html>\r\n");
+      // // Serial.println("</html>");
+    }
+    client.flush();
+    delay(10);
+    client.stop();
+  }
+}bool ledState = false;
+
+WiFiServer server(PORT);
+
+void bindWiFi() {
+  Serial.println("\nConnection to Wifi...");
+  WiFi.begin(SSID, PASSWORD);
+  while(WiFi.status() != WL_CONNECTED) {
+    Serial.println(".");
+    delay(PING_DELAY);
+  }
+  Serial.println("\nConnected!");
+}
+
+void configureGPIO() {
+  pinMode(LED_GPIO, OUTPUT);
+}
+
+void blinkLED() {
+  Serial.println("Turning on LED");
+  digitalWrite(LED_GPIO, HIGH);
+  delay(2000);
+  Serial.println("Turning off LED");
+  digitalWrite(LED_GPIO, LOW);
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  configureGPIO();
+  blinkLED();
+  Serial.println("\nInitializing....");
+
+  bindWiFi();
+  
+  server.begin();
+  Serial.println("\sIP Address:");
+  Serial.println(WiFi.localIP());
+}
+
 void loop() {
   // Listen for incoming clients
   WiFiClient client = server.available();
