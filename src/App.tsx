34bfa5237d56128c249622584b1d17045c4cf424
Nicholas Lediet <nicklediet@gmail.com>
2025-10-28 23:13:23 -0400

    Remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { useQuery } from '@tanstack/react-query'
 const esp32ServerUrl = import.meta.env.VITE_ESP32_SERVER_URL
